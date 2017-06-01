d880f6fd1d0b3c5e6fdd8a8cf8a2ab816822f56e
Alexandre Careil <a.careil@criteo.com>
2017-06-01 14:52:24 +0200

    Workflow tab improvements
    Change-Id: Ic93b3bbff0a8da0c39d57bf7d3798bdda2ef4b6e

--- examples/src/main/scala/com/criteo/cuttle/examples/HelloWorld.scala
+++ examples/src/main/scala/com/criteo/cuttle/examples/HelloWorld.scala
@@ -51,15 +51,103 @@ object HelloWorld {
         }
       }
 
-    val world = Job("world", daily(start, UTC)) { implicit e =>
+    val hello4 =
+      Job("hello4", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 4"
+          sleep 10
+        """.exec()
+      }
+
+    val hello5 =
+      Job("hello5", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 5"
+          sleep 10
+        """.exec()
+      }
+
+    val hello6 =
+      Job("hello6", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 6"
+          sleep 10
+        """.exec()
+      }
+
+    val hello7 =
+      Job("hello7", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 7"
+          sleep 10
+        """.exec()
+      }
+    val hello8 =
+      Job("hello8", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 8"
+          sleep 10
+        """.exec()
+      }
+    val hello9 =
+      Job("hello9", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 9"
+          sleep 10
+        """.exec()
+      }
+    val hello10 =
+      Job("hello10", hourly(start)) { implicit e =>
+        sh"""
+          echo "Hello 10"
+          sleep 10
+        """.exec()
+      }
+
+    val world1 = Job("world1", daily(start, UTC)) { implicit e =>
       sh"""
-        echo "World"
+        echo "World 1"
+        sleep 1
+      """.exec()
+    }
+
+    val world2 = Job("world2", daily(start, UTC)) { implicit e =>
+      sh"""
+        echo "World 2"
+        sleep 1
+      """.exec()
+    }
+
+    val world3 = Job("world3", daily(start, UTC)) { implicit e =>
+      sh"""
+        echo "World 3"
+        sleep 1
+      """.exec()
+    }
+
+    val langoustine = Job("langoustine", daily(start, UTC)) { implicit e =>
+      sh"""
+        echo "Langoustine"
+        sleep 1
+      """.exec()
+    }
+
+    val cuttle = Job("cuttle", daily(start, UTC)) { implicit e =>
+      sh"""
+        echo "Cuttle"
+        sleep 1
+      """.exec()
+    }
+
+    val scheduler = Job("scheduler", daily(start, UTC)) { implicit e =>
+      sh"""
+        echo "Scheduler"
         sleep 60
       """.exec()
     }
 
     Cuttle("Hello World") {
-      world dependsOn (hello1 and hello2 and hello3)
+      (langoustine and cuttle and scheduler) dependsOn ((world1 dependsOn (hello2 and hello4 and hello6 and hello7 and hello8 and hello10)) and (world2 dependsOn (hello1 and hello3 and hello5 and hello7 and hello9)) and (world3 dependsOn (hello7 and hello8 and hello9 and hello10)))
     }.run()
   }
 
