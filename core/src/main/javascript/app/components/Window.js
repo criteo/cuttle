a810fd3d1df4744a5ded2b71870459857515aeaa
Alexandre Careil <a.careil@criteo.com>
2017-06-08 14:57:47 +0200

    Fix style for execution window
    Change-Id: I33765e59e16744c50191e5bb038186b6e952876e

--- core/src/main/javascript/app/components/Window.js
+++ core/src/main/javascript/app/components/Window.js
@@ -20,18 +20,23 @@ const Window = ({ classes, title, children }: Props) => (
 
 const styles = {
   container: {
-    padding: "1em",
-    flex: "1",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    padding: "2em",
     display: "flex",
     flexDirection: "column",
-    position: "relative"
+    zIndex: "99999",
+    background: "rgba(53, 57, 68, 0.95)"
   },
   overlay: {
     background: "#ffffff",
     flex: "1",
     position: "relative",
     padding: "1em",
-    boxShadow: "0px 0px 15px 0px #799cb7",
+    boxShadow: "0px 0px 15px 0px #12202b",
     borderRadius: "2px",
     overflow: "hidden",
     display: "flex",
