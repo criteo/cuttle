2e52c5a54d6764f073e2df991b1208c23f389574
Vincent Guerci <v.guerci@criteo.com>
2017-06-15 12:10:32 +0200

    [backfills] improve form job selector / style cleanup

--- core/src/main/javascript/app/pages/BackfillCreate.js
+++ core/src/main/javascript/app/pages/BackfillCreate.js
@@ -3,7 +3,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import injectSheet from "react-jss";
-import goBack from "redux-url";
+import { goBack } from "redux-url";
 import CloseIcon from "react-icons/lib/md/close";
 import moment from "moment";
 
@@ -64,7 +64,7 @@ class BackfillCreate extends React.Component {
     return Promise.all(
       selectedJobs.map(job =>
         fetch(
-          `/api/timeseries/backfill?job=${job}&startDate=${dateFormat(start)}&endDate=${dateFormat(end)}&priority=${priority}`,
+          `/api/timeseries/backfill?name=${name}&job=${job}&startDate=${dateFormat(start)}&endDate=${dateFormat(end)}&priority=${priority}`,
           { method: "POST" }
         )
       )
@@ -81,6 +81,13 @@ class BackfillCreate extends React.Component {
     return (
       <Window title="Create Backfill">
         <CloseIcon className={classes.close} onClick={back} />
+        <div className={classes.filter}>
+          <JobSelector
+            workflow={workflow}
+            selected={selectedJobs}
+            onChange={this.selectJobs}
+          />
+        </div>
         <form onSubmit={this.createBackfill}>
           <FancyTable key="properties">
             <dt key="name_">Name:</dt>
@@ -127,19 +134,6 @@ class BackfillCreate extends React.Component {
             <dd key="create"><button>Create</button></dd>
           </FancyTable>
         </form>
-        <div className={classes.mainFilter}>
-          <JobSelector
-            workflow={workflow}
-            selected={selectedJobs}
-            placeholder={
-              <span>
-                {/*<FilterIcon className={classes.filterIcon}/>*/}
-                Filter on specific jobs...
-              </span>
-            }
-            onChange={this.selectJobs}
-          />
-        </div>
       </Window>
     );
   }
@@ -155,109 +149,8 @@ const styles = {
     cursor: "pointer",
     fontSize: "20px"
   },
-  definitions: {
-    margin: "-1em",
-    display: "flex",
-    flexFlow: "row",
-    flexWrap: "wrap",
-    fontSize: ".85em",
-    background: "rgba(189, 213, 228, 0.1)",
-    "& dt": {
-      flex: "0 0 150px",
-      textOverflow: "ellipsis",
-      overflow: "hidden",
-      padding: "0 1em",
-      boxSizing: "border-box",
-      textAlign: "right",
-      color: "#637686",
-      lineHeight: "2.75em"
-    },
-    "& dd": {
-      flex: "0 0 calc(100% - 150px)",
-      marginLeft: "auto",
-      textAlign: "left",
-      textOverflow: "ellipsis",
-      overflow: "hidden",
-      padding: "0",
-      boxSizing: "border-box",
-      lineHeight: "2.75em"
-    },
-    "& dd:nth-of-type(even), & dt:nth-of-type(even)": {
-      background: "#eef5fb"
-    }
-  },
-  failedLink: {
-    color: "#e91e63"
-  },
-  streams: {
-    flex: "1",
-    display: "flex",
-    background: "#23252f",
-    margin: "1em -1em -1em -1em",
-    position: "relative",
-
-    "& ul": {
-      flex: "1",
-      overflow: "scroll",
-      padding: "1em",
-      margin: "0",
-      listStyle: "none",
-      fontSize: ".85em",
-      lineHeight: "1.5em"
-    },
-
-    "& span": {
-      width: "150px",
-      color: "#747a88",
-      display: "inline-block",
-      marginRight: "-10px",
-      boxSizing: "border-box"
-    },
-
-    "& p": {
-      display: "inline-block",
-      margin: "0",
-      color: "#f1f1f1",
-      whiteSpace: "pre"
-    }
-  },
-  fullscreen: {
-    position: "fixed",
-    top: "0",
-    left: "0",
-    right: "0",
-    bottom: "0",
-    margin: "0",
-    zIndex: "99999"
-  },
-  fullscreenButton: {
-    cursor: "pointer",
-    color: "#fff",
-    fontSize: "22px",
-    position: "absolute",
-    right: "10px",
-    top: "10px"
-  },
-  autoScrollButton: {
-    cursor: "pointer",
-    color: "#fff",
-    fontSize: "22px",
-    position: "absolute",
-    right: "36px",
-    top: "10px"
-  },
-  activeAutoScroll: {
-    color: "#66cb63"
-  },
-  DEBUG: {
-    color: "#FFFF91 !important"
-  },
-  ERROR: {
-    color: "#FF6C60 !important"
-  },
-  // from App.js
-  mainFilter: {
-    zIndex: "2",
+  filter: {
+    margin: "-1em -1em 1em -1em", //TODO that is fishy ;(
     background: "#fff",
     height: "4em",
     lineHeight: "4em",
