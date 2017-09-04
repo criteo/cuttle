e167695712650ff4055f7054006bf35cfd7f5603
Arnaud Dufranne <dufrannea@gmail.com>
2017-09-04 11:13:21 +0200

    Removes query parameter for hard shutdown

--- core/src/main/scala/com/criteo/cuttle/App.scala
+++ core/src/main/scala/com/criteo/cuttle/App.scala
@@ -299,27 +299,24 @@ private[cuttle] case class App[S <: Scheduling](project: CuttleProject[S], execu
         Ok
       }
     }
-    case GET at url"/api/shutdown?gracePeriodSeconds=$gracePeriodSeconds&hard=$hard" => { implicit user =>
+    case GET at url"/api/shutdown?gracePeriodSeconds=$gracePeriodSeconds" => { implicit user =>
       import scala.concurrent.duration._
 
-      lazy val gracePeriod: Try[Long] = gracePeriodSeconds match {
+      val gracePeriod: Try[Long] = gracePeriodSeconds match {
         case "" => Success(300)
         case p => Try(p.toLong)
       }
 
-      hard match {
-        case "true" | "True" => {
+      gracePeriod match {
+        case Success(s) if s > 0 => {
+          executor.gracefulShutdown(Duration(s, SECONDS))
+          Ok
+        }
+        case Success(s) if s <= 0 => {
           executor.hardShutdown()
           Ok
         }
-        case _ =>
-          gracePeriod match {
-            case Success(s) if s > 0 => {
-              executor.gracefulShutdown(Duration(s, SECONDS))
-              Ok
-            }
-            case _ => BadRequest("gracePeriodSeconds should be a positive integer")
-          }
+        case _ => BadRequest("gracePeriodSeconds should be an integer")
       }
     }
   }
