1106f2cecadc7c4eade635862cf1ede6efd9b78b
Arnaud Dufranne <a.dufranne@criteo.com>
2017-09-13 14:56:44 +0200

    Recreates EventSource when connection lost

--- core/src/main/javascript/Utils.js
+++ core/src/main/javascript/Utils.js
@@ -3,16 +3,39 @@ export const listenEvents = (
   callback: (json: any) => void,
   error: ?(e: Event) => void
 ) => {
-  let minimumDelay = new Promise(resolve => setTimeout(resolve, 250));
-  let e = new EventSource(url);
+  const minimumDelay = new Promise(resolve => setTimeout(resolve, 250));
+  const keepOpenInterval = setInterval(keepOpen, 10000);
+  let eventSource = undefined;
   let open = true;
-  e.onmessage = msg =>
-    minimumDelay.then(() => open && callback(JSON.parse(msg.data)));
-  error && (e.onerror = e => error(e));
+  let isAlive = false;
+
+  function keepOpen(firstOpen) {
+    if (!isAlive && open) {
+      !firstOpen && error && error("connection lost");
+      eventSource && eventSource.close();
+      eventSource = new EventSource(url);
+      eventSource.onmessage = msg =>
+        minimumDelay.then(() => {
+          isAlive = true;
+          open && callback(JSON.parse(msg.data));
+        });
+
+      error &&
+        (eventSource.onerror = e => {
+          isAlive = true;
+          error(e);
+        });
+    }
+    isAlive = false;
+  }
+
+  keepOpen(true);
+
   return {
     close() {
       open = false;
-      e.close();
+      clearInterval(keepOpenInterval);
+      eventSource.close();
     }
   };
 };
