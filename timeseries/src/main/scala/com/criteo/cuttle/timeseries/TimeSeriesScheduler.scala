criteo/cuttle

commit 57729196b3000b3eca65c92edd35cafceb28d04f

Version support (#299)
* Add project version support in the calendar view

timeseries/src/main/scala/com/criteo/cuttle/timeseries/TimeSeriesScheduler.scala
@@ -215,7 +215,7 @@ private[timeseries] object Backfill {
   * @param end End instant of the partition to compute.
   * @param backfill If this execution is for a backfill, the [[Backfill]] informations are provided.
   */
-case class TimeSeriesContext(start: Instant, end: Instant, backfill: Option[Backfill] = None)
+case class TimeSeriesContext(start: Instant, end: Instant, backfill: Option[Backfill] = None, projectVersion: String = "")
     extends SchedulingContext {
 
   override def asJson: Json = TimeSeriesContext.encoder(this)
@@ -231,7 +231,7 @@ case class TimeSeriesContext(start: Instant, end: Instant, backfill: Option[Back
   def toInterval: Interval[Instant] = Interval(start, end)
 
   def compareTo(other: SchedulingContext) = other match {
-    case TimeSeriesContext(otherStart, _, otherBackfill) =>
+    case TimeSeriesContext(otherStart, _, otherBackfill, _) =>
       val priority: (Option[Backfill] => Int) = _.map(_.priority).getOrElse(0)
       val thisBackfillPriority = priority(backfill)
       val otherBackfillPriority = priority(otherBackfill)
@@ -291,7 +291,7 @@ object TimeSeries {
 
 private[timeseries] sealed trait JobState
 private[timeseries] object JobState {
-  case object Done extends JobState
+  case class Done(projectVersion: String) extends JobState
   case class Todo(maybeBackfill: Option[Backfill]) extends JobState
   case class Running(executionId: String) extends JobState
 
@@ -360,7 +360,7 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
           Right(
             interval2State
               .collect {
-                case (Interval(Finite(lo), Finite(hi)), Done) => (lo, hi)
+                case (Interval(Finite(lo), Finite(hi)), Done(_)) => (lo, hi)
               }
               .sortBy(_._1)
           ).right
@@ -475,7 +475,7 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
       // collect all periods that are intersecting with [start, end]
       val interval2State: List[(Interval[Instant], JobState)] = states(job).intersect(queryInterval).toList
       // in a Done state, and correct periodicity
-      interval2State.collect { case (Interval(Finite(lo), Finite(hi)), Done) =>
+      interval2State.collect { case (Interval(Finite(lo), Finite(hi)), Done(_)) =>
         (lo, hi) -> job
       }
     }.toList
@@ -595,7 +595,7 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
         val (stateSnapshot, newBackfills, completedBackfills) =
           collectCompletedJobs(_state(), _backfills(), completed)
 
-        val toRun = jobsToRun(workflow, stateSnapshot, Instant.now)
+        val toRun = jobsToRun(workflow, stateSnapshot, Instant.now, executor.projectVersion)
 
         _state() = stateSnapshot
         _backfills() = newBackfills
@@ -664,7 +664,7 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
     // update state with job statuses
     val newState = completed.foldLeft(state) {
       case (acc, (job, context, future)) =>
-        val jobState = if (future.value.get.isSuccess || isDone(state, job, context)) Done else Todo(context.backfill)
+        val jobState = if (future.value.get.isSuccess || isDone(state, job, context)) Done(context.projectVersion) else Todo(context.backfill)
         acc + (job -> acc(job).update(context.toInterval, jobState))
     }
 
@@ -676,7 +676,7 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
     (newState, notCompletedBackfills, backfills -- notCompletedBackfills)
   }
 
-  private[timeseries] def jobsToRun(workflow: Workflow, state0: State, now: Instant): List[Executable] = {
+  private[timeseries] def jobsToRun(workflow: Workflow, state0: State, now: Instant, projectVersion: String): List[Executable] = {
 
     val timerInterval = Interval(Bottom, Finite(now))
     val state = state0.mapValues(_.intersect(timerInterval))
@@ -699,7 +699,7 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
         .getOrElse(job, Set.empty)
         .map {
           case (_, parent, lbl) =>
-            val intervals: List[Interval[Instant]] = state(parent).collect { case Done => () }.toList.map(_._1)
+            val intervals: List[Interval[Instant]] = state(parent).collect { case Done(_) => () }.toList.map(_._1)
             val newIntervals = intervals.collect(applyDep(reverseDescr(lbl)))
             newIntervals.foldLeft(IntervalMap.empty[Instant, Unit])(_.update(_, ()))
         }
@@ -722,14 +722,14 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
         (interval, maybeBackfill) <- toRun.toList
         (lo, hi) <- job.scheduling.calendar.inInterval(interval, job.scheduling.maxPeriods)
       } yield {
-        (job, TimeSeriesContext(lo, hi, maybeBackfill))
+        (job, TimeSeriesContext(lo, hi, maybeBackfill, projectVersion))
       }
     }
   }
 
-  private[timeseries] def forceSuccess(job: TimeSeriesJob, interval: Interval[Instant]): Unit = atomic { implicit txn =>
+  private[timeseries] def forceSuccess(job: TimeSeriesJob, interval: Interval[Instant], projectVersion: String): Unit = atomic { implicit txn =>
     val jobState = _state().apply(job)
-    _state() = _state() + (job -> jobState.update(interval, Done))
+    _state() = _state() + (job -> jobState.update(interval, Done(projectVersion)))
   }
 
   private def getRunningBackfillsSize(jobs: Set[String]) = {

core/src/main/scala/com/criteo/cuttle/Executor.scala
@@ -184,8 +184,9 @@ case class Execution[S <: Scheduling](
   context: S#Context,
   streams: ExecutionStreams,
   platforms: Seq[ExecutionPlatform],
-  projectName: String
+  projectVersion: String
 )(implicit val executionContext: SideEffectThreadPool) {
+
 
   private var waitingSeconds = 0
   private[cuttle] var startTime: Option[Instant] = None
@@ -380,7 +381,7 @@ private[cuttle] object ExecutionPlatform {
 class Executor[S <: Scheduling] private[cuttle] (val platforms: Seq[ExecutionPlatform],
                                                  xa: XA,
                                                  logger: Logger,
-                                                 projectName: String)(implicit retryStrategy: RetryStrategy)
+                                                 val projectVersion: String)(implicit retryStrategy: RetryStrategy)
     extends MetricProvider[S] {
 
   import ExecutionStatus._
@@ -863,7 +864,7 @@ class Executor[S <: Scheduling] private[cuttle] (val platforms: Seq[ExecutionPla
                       def writeln(str: CharSequence) = ExecutionStreams.writeln(nextExecutionId, str)
                     },
                     platforms,
-                    projectName
+                    projectVersion
                   )
                   val promise = Promise[Completed]
 

core/src/test/scala/com/criteo/cuttle/ExecutorSpec.scala
@@ -3,19 +3,19 @@ package com.criteo.cuttle
 import java.sql.{Connection, ResultSet}
 
 import scala.concurrent.Future
-
 import cats.effect.IO
 import com.mysql.cj.jdbc.PreparedStatement
 import doobie.util.transactor.Transactor
 import org.mockito.Matchers._
 import org.mockito.Mockito._
 import org.scalatest.FunSuite
-
 import com.criteo.cuttle.ThreadPools.Implicits.sideEffectThreadPool
 import com.criteo.cuttle.ThreadPools._
+
 import com.criteo.cuttle.Metrics.Prometheus
 
 class ExecutorSpec extends FunSuite with TestScheduling {
+
   test("Executor should return metrics aggregated by job and tag") {
     val connection: Connection = {
       val mockConnection = mock(classOf[Connection])
@@ -30,7 +30,7 @@ class ExecutorSpec extends FunSuite with TestScheduling {
       Seq.empty,
       xa = Transactor.fromConnection[IO](connection).copy(strategy0 = doobie.util.transactor.Strategy.void),
       logger,
-      "test_project"
+      "test_version"
     )(RetryStrategy.ExponentialBackoffRetryStrategy)
 
     testExecutor.updateFinishedExecutionCounters(buildExecutionForJob(fooJob), "success")
@@ -123,7 +123,7 @@ class ExecutorSpec extends FunSuite with TestScheduling {
         override private[cuttle] def writeln(str: CharSequence): Unit = ???
       },
       platforms = Seq.empty,
-      "foo-project"
+      "test_version"
     )
 
   private val fooTag = Tag("foo")

examples/src/main/scala/com/criteo/cuttle/examples/HelloTimeSeries.scala
@@ -109,7 +109,7 @@ object HelloTimeSeries {
     }
 
     // Finally we bootstrap our cuttle project.
-    CuttleProject("Hello TimeSeries", env = ("dev", false)) {
+    CuttleProject("Hello World", version = "123", env = ("dev", false)) {
       // Any cuttle project contains a Workflow to execute. This Workflow is composed from jobs
       // or from others smaller Workflows.
       world dependsOn (hello1 and hello2 and hello3)

timeseries/src/main/javascript/app/pages/CalendarFocus.js
@@ -53,6 +53,7 @@ type JobPeriodSlot =
       period: Period,
       status: "done" | "running" | "todo",
       backfill: boolean,
+      version: string,
       aggregated: false
     }
   | {
@@ -71,7 +72,8 @@ type Stats = {
 type State = {
   data: ?Stats,
   query: ?string,
-  eventSource: ?any
+  eventSource: ?any,
+  showVersion: boolean
 };
 
 // Static/format helpers
@@ -82,6 +84,19 @@ let RIGHT_MARGIN = 50;
 let ROW_HEIGHT = 20;
 let PADDING = 5;
 let MARGIN = 1;
+let VERSION_RECT_SIZE = 0.4 * ROW_HEIGHT;
+let LEGEND_WIDTH = 80;
+
+let versionPointer = 0;
+const versionColorMap = new Map();
+const versionsColor = [ 
+  "#ff9900",
+  "#00e6e6",
+  "#7a00cc",
+  "#996600",
+  "#e6e600",
+  "#0000cc",
+  "#b300b3"];
 
 let formatDate = (date: string) =>
   moment(date).utc().format("YYYY-MM-DD HH:mm");
@@ -139,6 +154,20 @@ const jobPeriodsHelper = (x1, x2, showExecutions, drillDown) => ({
     error ? "#e91e63" : colorScale(completion)
 });
 
+// convert a version string to a specific color
+const stringToColor = function(str) {
+  // if this version was already associated with a color, return it
+  if (versionColorMap.has(str)) {
+    return versionColorMap.get(str);
+  } else {
+    // chose the next color available in the list
+    let newColor = versionsColor[versionPointer % (versionsColor.length - 1)];
+    versionColorMap.set(str, newColor);
+    versionPointer++;
+    return newColor;
+  }
+}
+
 // method drawing all the cases of color for job periods (for specific jobs in the calendar focus)
 // A summary period is an aggregated period with a special tooltip (not dedicated to a job)
 const drawJobPeriods = (
@@ -147,6 +176,7 @@ const drawJobPeriods = (
   x1,
   x2,
   showExecutions,
+  showVersion,
   drillDown,
   summary = false
 ) => {
@@ -219,20 +249,43 @@ const drawJobPeriods = (
     .attr("fill", "#bb65ca")
     .attr("x", MARGIN)
     .attr("y", ROW_HEIGHT - 3);
+
+    // If the version box is checked, draw a color rectangle for each done job
+  newPeriodSlot
+    .selectAll("rect.version")
+    .data(d => showVersion && d.version ? [d] : [], k => k.period.start)
+    .enter()
+    .append("rect")
+    .attr("class", "version")
+    .attr("width", VERSION_RECT_SIZE)
+    .attr("height", VERSION_RECT_SIZE)
+    .attr("fill", d => stringToColor(d.version))
+    .style("opacity", 1.0)
+    .attr("x", MARGIN)
+    .attr("y", 0);
 };
 
 class CalendarFocus extends React.Component<Props, State> {
   vizContainer: any;
   summarySvgContainer: any;
   detailsSvgContainer: any;
+  legendContainer: any;
 
   constructor(props: Props) {
     super(props);
     this.state = {
       data: null,
       query: null,
-      eventSource: null
+      eventSource: null,
+      showVersion: false
     };
+  }
+
+
+  toggleShowVersion() {
+    this.setState({
+      showVersion : !this.state.showVersion
+    })
   }
 
   listenForUpdates(props: Props) {
@@ -255,10 +308,11 @@ class CalendarFocus extends React.Component<Props, State> {
   drawViz(props: Props, state: State) {
     let { data } = state;
     let { classes, start, end, workflow } = props;
+
     if (data && data.summary.length) {
       let { summary, jobs } = data;
-
       let width = this.vizContainer.clientWidth;
+      
       let summarySvg = d3
         .select(this.summarySvgContainer)
         .attr("width", width)
@@ -313,6 +367,7 @@ class CalendarFocus extends React.Component<Props, State> {
           x1,
           x2,
           this.props.showExecutions,
+          state.showVersion,
           this.props.drillDown,
           true
         );
@@ -345,7 +400,7 @@ class CalendarFocus extends React.Component<Props, State> {
         newJobTimeline
           .selectAll("g.periodSlot")
           .data(
-            job =>
+            job => 
               _.map(jobs[job.id], p => ({
                 ...p,
                 jobName: job.name,
@@ -360,7 +415,9 @@ class CalendarFocus extends React.Component<Props, State> {
             x1,
             x2,
             this.props.showExecutions,
-            this.props.drillDown
+            state.showVersion,
+            this.props.drillDown,
+            false
           );
       };
 
@@ -373,6 +430,56 @@ class CalendarFocus extends React.Component<Props, State> {
         .data(jobInfos, job => job.id)
         .enter()
         .call(drawJobs);
+
+
+      // display legend for every distinct project version
+      const versions = Object.keys(jobs).map(k => 
+        jobs[k].map(periodSlot => 
+          !periodSlot.aggregated ? periodSlot.version : undefined
+          )
+        );
+      const distinctVersions = [...new Set([].concat(...versions))].filter(version => version !== "" && version !== undefined);
+
+      const versionNodes = d3
+      .select(this.legendContainer)
+      .attr("width", axisWidth)
+      .selectAll("g.jobVersionLegend")
+      .data(state.showVersion ? distinctVersions: [])
+
+
+      const versionGroup = versionNodes
+      .enter()
+      .append("g")
+      .attr("width", LEGEND_WIDTH)
+      .attr("height", 20)
+      .attr("class", "jobVersionLegend")
+      .attr("text-anchor", "end")
+      .attr(
+        "transform",
+        (d, i) =>
+        `translate(${axisWidth - LEGEND_WIDTH - labelWidth}, ${i * (ROW_HEIGHT + 2 * MARGIN)})`
+      )
+      
+      versionGroup
+      .append("rect")
+      .attr("class", "legend")
+      .attr("width", VERSION_RECT_SIZE)
+      .attr("height", VERSION_RECT_SIZE)
+      .attr("x", 0)
+      .attr("y", 10)
+      .attr("fill", d => stringToColor(d));
+      
+      versionGroup
+      .append("text")
+      .attr("class","legendText")
+      .attr("x", LEGEND_WIDTH)
+      .attr("y", 20)
+      .text(d => d);
+
+      versionNodes
+      .exit()
+      .remove();
+      
     }
     ReactTooltip.rebuild();
 
@@ -467,6 +574,16 @@ class CalendarFocus extends React.Component<Props, State> {
             {" "}
             UTC
           </span>
+        </h1>
+        <div>
+          <div className={classes.showVersion}>
+            <input
+              name="versionCheckBox"
+              type="checkbox"
+              checked={this.state.showVersion}
+              onChange={() => this.toggleShowVersion()} 
+            />
+          </div>
           <div className={classes.timeControl}>
             <ArrowPrevious
               onClick={() => this.timeShift("back")}
@@ -487,7 +604,7 @@ class CalendarFocus extends React.Component<Props, State> {
               className="button"
             />
           </div>
-        </h1>
+        </div>
         {data && data.summary.length
           ? <div className={classes.graph} ref={r => (this.vizContainer = r)}>
               <div className={classes.summarySvg}>
@@ -498,6 +615,9 @@ class CalendarFocus extends React.Component<Props, State> {
               </div>
               <div className={classes.detailsSvg}>
                 <svg ref={r => (this.detailsSvgContainer = r)} />
+                <div className={classes.legendContainer}>
+                  <svg ref={r => (this.legendContainer = r)}/>
+                </div>
               </div>
               <ReactTooltip
                 className={classes.tooltip}
@@ -538,6 +658,18 @@ const styles = {
     fontSize: "0.8em",
     float: "right",
     textAlign: "right",
+    height: "30px",
+    width: "160px",
+    color: "#607e96",
+    "& .button": {
+      cursor: "pointer",
+      margin: "0 0.5em"
+    }
+  },
+  showVersion: {
+    fontSize: "0.8em",
+    float: "left",
+    textAlign: "left",
     height: "30px",
     width: "160px",
     color: "#607e96",
@@ -608,6 +740,9 @@ const styles = {
     paddingTop: "30px",
     flex: "1",
     overflowX: "hidden"
+  },
+  legendContainer: {
+    float: "right"
   },
   tooltip: {
     backgroundColor: "#2b3346 !important",

timeseries/src/main/scala/com/criteo/cuttle/timeseries/CuttleProject.scala
@@ -37,7 +37,7 @@ class CuttleProject private[cuttle] (
     paused: Boolean = false
   ): Unit = {
     val xa = CuttleDatabase.connect(databaseConfig)
-    val executor = new Executor[TimeSeries](platforms, xa, logger, name)(retryStrategy)
+    val executor = new Executor[TimeSeries](platforms, xa, logger, version)(retryStrategy)
 
     if (paused) {
       logger.info("Pausing workflow")
@@ -71,7 +71,7 @@ class CuttleProject private[cuttle] (
     retryStrategy: RetryStrategy = RetryStrategy.ExponentialBackoffRetryStrategy
   ): (Service, () => Unit) = {
     val xa = CuttleDatabase.connect(databaseConfig)
-    val executor = new Executor[TimeSeries](platforms, xa, logger, name)(retryStrategy)
+    val executor = new Executor[TimeSeries](platforms, xa, logger, version)(retryStrategy)
 
     val startScheduler = () => {
       logger.info("Start workflow")

timeseries/src/main/scala/com/criteo/cuttle/timeseries/Database.scala
@@ -139,7 +139,7 @@ private[timeseries] object Database {
         (job.id, im.toList.filter {
           case (_, jobState) =>
             jobState match {
-              case Done    => true
+              case Done(_) => true
               case Todo(_) => true
               case _       => false
             }

timeseries/src/main/scala/com/criteo/cuttle/timeseries/TimeSeriesApp.scala
@@ -382,9 +382,10 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
     val period: Interval[Instant]
     val backfill: Boolean
     val aggregated: Boolean
+    val version: String
   }
 
-  private case class JobExecution(period: Interval[Instant], status: String, backfill: Boolean)
+  private case class JobExecution(period: Interval[Instant], status: String, backfill: Boolean, version: String)
       extends ExecutionPeriod {
     override val aggregated: Boolean = false
   }
@@ -392,7 +393,8 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
   private case class AggregatedJobExecution(period: Interval[Instant],
                                             completion: Double,
                                             error: Boolean,
-                                            backfill: Boolean)
+                                            backfill: Boolean,
+                                            version: String = "")
       extends ExecutionPeriod {
     override val aggregated: Boolean = true
   }
@@ -404,11 +406,12 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
       val coreFields = List(
         "period" -> executionPeriod.period.asJson,
         "backfill" -> executionPeriod.backfill.asJson,
-        "aggregated" -> executionPeriod.aggregated.asJson
+        "aggregated" -> executionPeriod.aggregated.asJson,
+        "version" -> executionPeriod.version.asJson
       )
       val finalFields = executionPeriod match {
-        case JobExecution(_, status, _) => ("status" -> status.asJson) :: coreFields
-        case AggregatedJobExecution(_, completion, error, _) =>
+        case JobExecution(_, status, _, _) => ("status" -> status.asJson) :: coreFields
+        case AggregatedJobExecution(_, completion, error, _, _) =>
           ("completion" -> completion.asJson) :: ("error" -> error.asJson) :: coreFields
       }
       Json.obj(finalFields: _*)
@@ -457,7 +460,7 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
               .collect { case (itvl, Todo(maybeBackfill)) => (itvl, maybeBackfill) }
             (lo, hi) <- calendar.split(interval)
           } yield {
-            val context = TimeSeriesContext(calendar.truncate(lo), calendar.ceil(hi), maybeBackfill)
+            val context = TimeSeriesContext(calendar.truncate(lo), calendar.ceil(hi), maybeBackfill, executor.projectVersion)
             ExecutionLog("", job.id, None, None, context.asJson, ExecutionTodo, None, 0)
           }
         val throttledExecutions = executor.allFailingExecutions
@@ -494,7 +497,7 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
               .collect { case (itvl, Todo(maybeBackfill)) => (itvl, maybeBackfill) }
             (lo, hi) <- calendar.split(interval)
           } yield {
-            val context = TimeSeriesContext(calendar.truncate(lo), calendar.ceil(hi), maybeBackfill)
+            val context = TimeSeriesContext(calendar.truncate(lo), calendar.ceil(hi), maybeBackfill, executor.projectVersion)
             ExecutionLog("", parentJob.id, None, None, context.asJson, ExecutionTodo, None, 0)
           }
 
@@ -571,6 +574,11 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
               }
             }
 
+        def getVersionFromState(jobState: JobState): String = jobState match {
+          case Done(version) => version
+          case _ => ""
+        }
+
         def getStatusLabelFromState(jobState: JobState, job: Job[TimeSeries]): String =
           jobState match {
             case Running(e) =>
@@ -582,7 +590,7 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
                 "paused"
               else "running"
             case Todo(_) => "todo"
-            case Done    => "successful"
+            case Done(_) => "successful"
           }
 
         val jobTimelines =
@@ -605,7 +613,7 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
               if (calendarView.aggregationFactor == 1)
                 jobStatesOnIntervals match {
                   case (_, state) :: Nil =>
-                    Some(JobExecution(interval, getStatusLabelFromState(state, job), inBackfill))
+                    Some(JobExecution(interval, getStatusLabelFromState(state, job), inBackfill, getVersionFromState(state)))
                   case _ => None
                 } else
                 jobStatesOnIntervals match {
@@ -897,7 +905,8 @@ private[timeseries] case class TimeSeriesApp(project: CuttleProject, executor: E
           job <- Try(project.jobs.all.find(_.id == jobId).getOrElse(throw new Exception(s"Unknow job $jobId")))
         } yield {
           val requestedInterval = Interval(startDate, endDate)
-          scheduler.forceSuccess(job, requestedInterval)
+          //self.forceSuccess(job, requestedInterval, executor.cuttleProject.version)
+          scheduler.forceSuccess(job, requestedInterval, executor.projectVersion)
           val runningExecutions = executor.runningExecutions.filter(e => e._1.job.id == jobId).map(_._1)
           val failingExecutions = executor.allFailingExecutions.filter(_.job.id == jobId)
           val executions = runningExecutions ++ failingExecutions

commit 78bbaa4e76818d4927d5b4d8ece1cdabe2f4223c

Fixes bug that prevented backfills from being marked as finished
Introduced by https://github.com/criteo/cuttle/pull/299

timeseries/src/main/scala/com/criteo/cuttle/timeseries/TimeSeriesScheduler.scala
@@ -736,7 +736,10 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
                                                backfills: Set[Backfill],
                                                completed: Set[Run]): (State, Set[Backfill], Set[Backfill]) = {
     def isDone(state: State, job: TimeSeriesJob, context: TimeSeriesContext): Boolean =
-      state.apply(job).intersect(context.toInterval).toList.forall { case (_, state) => state == Done }
+      state.apply(job).intersect(context.toInterval).toList.forall {
+        case (_, Done(_)) => true
+        case _ => false
+      }
 
     // update state with job statuses
     val newState = completed.foldLeft(state) {
@@ -747,9 +750,11 @@ case class TimeSeriesScheduler(logger: Logger) extends Scheduler[TimeSeries] {
 
     val notCompletedBackfills = backfills.filter { bf =>
       val itvl = Interval(bf.start, bf.end)
-      bf.jobs.exists(job => newState(job).intersect(itvl).toList.exists(_._2 != Done))
+      bf.jobs.exists(job => newState(job).intersect(itvl).toList.exists(_._2 match {
+        case Done(_) => true
+        case _ => false
+      }))
     }
-
     (newState, notCompletedBackfills, backfills -- notCompletedBackfills)
   }
 