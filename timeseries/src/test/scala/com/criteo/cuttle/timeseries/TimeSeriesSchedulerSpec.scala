bf45117bfe09f79998608589f436093bf6bc4fbf
Arnaud Dufranne <dufrannea@gmail.com>
2019-01-25 16:32:43 +0100

    Adds support for periods of n hours
    Periods should be strict dividers of 24.

--- timeseries/src/test/scala/com/criteo/cuttle/timeseries/TimeSeriesSchedulerSpec.scala
+++ timeseries/src/test/scala/com/criteo/cuttle/timeseries/TimeSeriesSchedulerSpec.scala
@@ -1,6 +1,6 @@
 package com.criteo.cuttle.timeseries
 
-import java.time.Duration
+import java.time.{Duration, Instant}
 
 import scala.concurrent.Future
 
@@ -94,5 +94,43 @@ class TimeSeriesSchedulerSpec extends FunSuite with TestScheduling {
         (testJob, TimeSeriesContext(date"2017-03-25T02:00:00Z", date"2017-03-25T03:00:00Z", None, "last_version")),
         (parentTestJob, TimeSeriesContext(date"2017-03-25T04:00:00Z", date"2017-03-25T05:00:00Z", None, "last_version"))
       )))
+  }
+
+  val oneDayInterval = Interval(Instant.parse("2019-01-01T00:00:00Z"), Instant.parse("2019-01-02T00:00:00Z"))
+  val start = Instant.parse("2019-01-01T00:00:00Z")
+
+  test("Generate right n-hours periods") {
+
+    def checkPeriod(period: Int) = {
+      val allIntervals = nhourly(period, start)
+        .calendar.inInterval(oneDayInterval, 1).toList
+
+      assert(allIntervals.size.equals(24/period))
+
+      assert(allIntervals.forall({ case (lo, hi) =>
+        Duration.between(lo, hi) == Duration.ofHours(period)
+      }))
+
+      assert(allIntervals.zip(allIntervals.drop(1)).forall({ case (lo, hi) =>
+        Duration.between(lo._2, hi._1) == Duration.ofHours(0)
+      }))
+    }
+
+    List(1,2,3,4,6,8,12).map(i => checkPeriod(i))
+  }
+
+  test("NHourly can only be created for dividers of 24") {
+    assertThrows[IllegalArgumentException](nhourly(-1, start))
+    assertThrows[IllegalArgumentException](nhourly(0, start))
+
+    List(1,2,3,4,6,8,12).foreach(i => {
+      nhourly(i, start)
+    })
+    List(5,7,9,10,11,13,15,16,17,18,19,20,21,22,23).foreach(i => {
+      assertThrows[IllegalArgumentException](nhourly(i, start))
+    })
+
+    assertThrows[IllegalArgumentException](nhourly(24, start))
+    assertThrows[IllegalArgumentException](nhourly(25, start))
   }
 }
