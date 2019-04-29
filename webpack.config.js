598f36d5fd3a22e53620af60e937b3844d208bc2
Alexey ERYSHEV <a.eryshev@criteo.com>
2019-04-29 15:35:24 +0200

    update JS dependencies
    -babel 7
    -webpack 5

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const FlowStatusWebpackPlugin = require("flow-status-webpack-plugin");
-const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
 const outputPath = path.resolve(
   __dirname,
@@ -39,8 +38,8 @@ module.exports = {
             loader: "babel-loader",
             options: {
               cacheDirectory: true,
-              presets: ["react", "es2015", "stage-0"],
-              plugins: []
+              presets: ["@babel/env", "@babel/flow", "@babel/react"],
+              plugins: ["@babel/plugin-proposal-class-properties"]
             }
           }
         ],
@@ -66,9 +65,6 @@ module.exports = {
     new FlowStatusWebpackPlugin({
       binaryPath: path.resolve(__dirname, "node_modules/.bin/flow"),
       failOnError: true
-    }),
-    new UglifyJSPlugin({
-      mangle: false
     })
   ]
 };
